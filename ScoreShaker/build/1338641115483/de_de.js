425528d63e3d715bab27eaa1568d14f3debec631
its@time2dev.com <hano@Marcos-MacBook-Pro.local>
2012-06-02 14:45:58 +0200

    a builded version

---
+++ ScoreShaker/build/1338641115483/de_de.js
@@ -0,0 +1,33 @@
+// ==========================================================================
+// The M-Project - Mobile HTML5 Application Framework
+// Generated with: Espresso 
+//
+// Project: ScoreShaker
+// I18N: de_de
+// ==========================================================================
+
+/*
+ * Usage in a project:
+ *
+ * label: M.LabelView.design({
+ *     value: M.I18N.l('_welcome')
+ * })
+ *
+ */
+
+
+M.I18N.de_de = {
+
+    'key': 'Wert',
+
+    '_welcome': 'Willkommen',
+
+    'back': 'zurück',
+    'tmpUrl': 'http://the-m-project.org/',
+    'shake': 'shake',
+    'coded': 'Proudly presented by <br /> The-M-Project',
+
+
+    'undefined' : 'undefined'
+
+}
---
+++ ScoreShaker/build/1338641115483/en_en.js
@@ -0,0 +1,32 @@
+// ==========================================================================
+// The M-Project - Mobile HTML5 Application Framework
+// Generated with: Espresso 
+//
+// Project: ScoreShaker
+// I18N: en_us
+// ==========================================================================
+
+/*
+ * Usage in a project:
+ *
+ * label: M.LabelView.design({
+ *     value: M.I18N.l('_welcome')
+ * })
+ *
+ */
+
+
+M.I18N.en_en = {
+
+    'key': 'Value',
+
+    '_welcome': 'Welcome',
+
+    'back': 'back',
+    'tmpUrl': 'http://the-m-project.org/',
+    'shake': 'shake',
+    'coded': 'Proudly presented by <br /> The-M-Project',
+
+    'undefined' : 'undefined'
+
+}
